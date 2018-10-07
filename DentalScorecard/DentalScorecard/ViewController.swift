3a31a1d28a3f80568226b023001ca0887d82c8d0
Julian Cearley <c3arl3y@julians-macbook-pro.local>
2018-10-07 17:41:36 -0400

    First Commit of the dental scorecard

---
+++ DentalScorecard/DentalScorecard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DentalScorecard
+//
+//  Created by Julian Cearley on 10/7/18.
+//  Copyright © 2018 Julian Cearley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
